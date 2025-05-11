d81469b7ea2d1cefb5b1864606f884fbfd90d115
adika-sulis <angeloadam@elg-bp.edu.hu>
2025-05-11 15:31:10 +0200

    added: blur and changed dark-mode

--- js/modal.js
+++ js/modal.js
@@ -1,9 +1,14 @@
 window.onload = function () {
   setTimeout(function () {
     document.getElementById("alert").style.display = "flex";
-  }, 1000);
+  }, 500);
 }
 
   function closeModal() {
     document.getElementById("alert").style.display = "none";
+
+    var elements = document.getElementsByClassName("blurred");
+  for (var i = 0; i < elements.length; i++) {
+    elements[i].classList.toggle("blurred");
+  }
   }
