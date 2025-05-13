a301f4ea05ada57c5420e3f553fbf4dc12adaf38
adika-sulis <angeloadam@elg-bp.edu.hu>
2025-05-13 18:40:46 +0200

    added: button formatting in light and dark-mode with js function

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -3,4 +3,15 @@ function toggleDark() {
   for (var i = 0; i < elements.length; i++) {
     elements[i].classList.toggle("dark");
   }
+  const buttons = document.querySelectorAll(".btn");
+
+  buttons.forEach(btn => {
+    if (btn.classList.contains("btn-light")) {
+      btn.classList.remove("btn-light");
+      btn.classList.add("btn-dark");
+    } else {
+      btn.classList.remove("btn-dark");
+      btn.classList.add("btn-light");
+    }
+  });
 }
