09bafbce7161670478c6302044c1bc29e78e7056
adika-sulis <angeloadam@elg-bp.edu.hu>
2026-06-29 17:54:49 +0200

    accounts?? peak

---
+++ account/account.js
@@ -0,0 +1,13 @@
+function logout() {
+    setCookie("hash", "", -1);
+    location.href("./login/index.php");
+}
+
+function setCookie(name, value, days) {
+    const expires = new Date(Date.now() + days * 864e5).toUTCString();
+    document.cookie = name + "=" + encodeURIComponent(value) + "; expires=" + expires + "; path=/";
+}
+
+function getCookie(name) {
+    return document.cookie.split("; ").find(row => row.startsWith(name + "="))?.split("=")[1];
+}
