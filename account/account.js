64f9d45567b29a82143b17224fb037a53f5719f6
adika-sulis <angeloadam@elg-bp.edu.hu>
2026-06-30 03:32:36 +0200

    fixes ig

--- account/account.js
+++ account/account.js
@@ -1,6 +1,15 @@
 function logout() {
     setCookie("hash", "", -1);
-    location.href("./login/index.php");
+    location.href = "https://adika-sulis.rf.gd/account/login/index.php";
+}
+
+function deleteAccount() {
+    var deleteDiv = document.getElementById("delete")
+    deleteDiv.innerHTML = `<h1>Ez a művelet <strong>nem vonható vissza</strong>!</h1>
+    <form method="POST" action="">
+        <input type="hidden" name="action" value="delete">
+        <button class="btn btn-danger">Fiókom törlése</button>
+    </form> `
 }
 
 function setCookie(name, value, days) {
